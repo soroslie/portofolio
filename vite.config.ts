e6203bad00b767974cf4b7035e81ced339f62272
soros.lie <soros.lie@seamoney.com>
2023-07-21 08:35:50 +0700

    edit config

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://soroslie.github.io/soros-lie',
+  base: '/soros-lie/',
   plugins: [react()],
 })
