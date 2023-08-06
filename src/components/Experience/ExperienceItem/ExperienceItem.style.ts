e99e1d8b3919fea90a9783397178cd68f33c9750
Soros Lie <soroslie75@gmail.com>
2023-08-06 09:34:13 +0700

    add project number respondsive

--- src/components/Experience/ExperienceItem/ExperienceItem.style.ts
+++ src/components/Experience/ExperienceItem/ExperienceItem.style.ts
@@ -1,5 +1,6 @@
 import styled from 'styled-components'
 import ColorPalletes from '../../../utils/color'
+import { MediaScreen } from '../../../utils/mediaScreen'
 
 export const Container = styled.div`
   width: 200px;
@@ -15,9 +16,17 @@ export const Container = styled.div`
   justify-content: center;
   align-content: stretch;
   color: ${ColorPalletes.whitePrimary};
+  @media only screen and (max-width: ${MediaScreen.tablet}) {
+    width: 100px;
+    min-width: 100px;
+    padding: 1.5rem 1.5rem 1rem 1.5rem;
+  }
 `
 export const Icon = styled.div`
   font-size: 40px;
+  @media only screen and (max-width: ${MediaScreen.tablet}) {
+    font-size: 30px;
+  }
 `
 export const Heading = styled.h2`
   margin-top: 0.25rem;
@@ -26,6 +35,10 @@ export const Heading = styled.h2`
   line-height: 1;
   font-weight: 600;
   text-transform: capitalize;
+  @media only screen and (max-width: ${MediaScreen.tablet}) {
+    min-height: 36px;
+    font-size: 18px;
+  }
 `
 export const SubHeading = styled.h2`
   margin-top: 0.25rem;
